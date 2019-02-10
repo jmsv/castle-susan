9042157bde3828850de9f9fb828c6d7bf1e11eb2
James Vickery <dev@jamesvickery.net>
2019-02-10 03:45:24 +0000

    castle driver

---
+++ castle-susan-driver/castle-susan-driver.ino
@@ -0,0 +1,96 @@
+#include <RedBot.h>
+#include <RedBotSoftwareSerial.h>
+
+#define BEEPER 10
+
+RedBotMotors motor;
+
+void setup() {
+  motor.stop();
+  // emergency stop: uncomment below
+  // return;
+
+  Serial.begin(9600);
+  Serial.print("started serial server");
+
+  ok();
+}
+
+void loop() {
+  while (Serial.available() > 0) {
+    process_cmd();
+  }
+}
+
+void process_cmd() {
+  char cmd = Serial.read();
+
+  Serial.print("\n");
+  Serial.print(cmd);
+
+  switch (cmd) {
+    // Drawbridge up and down
+    case 'u':
+      drawb_up(); break;
+    case 'd':
+      drawb_down(); break;
+
+    // Drawbridge calibrate
+    case '+':
+      drawb_calibrate(1); break;
+    case '-':
+      drawb_calibrate(-1); break;
+
+    // Buzzer sounds
+    case 'k':
+      ok(); break;
+    case 'b':
+      annoying_noise(); break;
+
+    // Fallback to playing annoying error noise
+    default:
+      unknown_cmd(); break;
+  }
+}
+
+
+void drawb_calibrate(int sign) {
+  motor.drive(sign * 100);
+  delay(100);
+  motor.stop();
+}
+
+void drawb_down() {
+  motor.drive(-255);
+  delay(2800);
+  motor.stop();
+}
+
+void drawb_up() {
+  motor.drive(255);
+  delay(2800);
+  motor.stop();
+}
+
+void annoying_noise() {
+  return; // shut up
+  for (int i = 9000; i > 99; i -= 30) {
+    tone(BEEPER, i, 5);
+    delay(3);
+  }
+  for (int i = 100; i < 6000; i += 10) {
+    tone(BEEPER, i, 800);
+    delay(5);
+  }
+}
+
+void ok() { // weird noise thing
+  for (int i = 2; i < 3; i++) {
+    tone(BEEPER, i * 1000, 100);
+    delay(250);
+  }
+}
+
+void unknown_cmd() {
+  tone(BEEPER, 4000, 1500);
+}
